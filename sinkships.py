6673ddec083b19515c024e778617caa6e809c81f
mrdrawin <mrdrawin@abv.bg>
2023-09-16 19:10:51 +0200

    sink ships initial commit

---
+++ sinkships.py
@@ -0,0 +1,29 @@
+
+
+class Main: 
+    _generatedField:[] = [["x"]]
+    listIndex:int = 0
+    
+    def __init__(self) :
+        pass
+
+    def generateField(self):
+        sideA:int = int(input("How many rows?"))
+        sideB:int = int(input("How many columns?"))
+
+        while len(self._generatedField) < sideA:
+            self._generatedField.append(globals()["list_" + str(self.listIndex) + "_"])
+            self.listIndex += 1 
+            while len(globals()["list_" + str(self.listIndex) + "_"]) < sideB:
+                globals()["list_" + str(self.listIndex) + "_"].append("x")
+    
+    
+    def getField(self):
+        return self._generatedField
+    '''
+    def getIndex(self):
+        return self.listIndex
+    '''
+
+main = Main()
+
