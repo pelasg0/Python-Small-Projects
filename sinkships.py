ce0bd38d59535e74690cc8c780a78ee13993daca
mrdrawin <mrdrawin@abv.bg>
2023-09-22 22:33:19 +0200

    generate ship blueprint

--- sinkships.py
+++ sinkships.py
@@ -1,15 +1,15 @@
 
 
 class Main: 
-    _generatedField = []
-    listIndex:int = 0
+    _generatedField: list = []
+    #listIndex:int = 0
     
     def __init__(self):
         pass
 
-    def generateField(self, _generatedField):
-        self.rowAmount = int(input("Enter Amount of Rows: "))
-        self.columnAmount = int(input("Enter Amount of Items on a Row: "))
+    def generateField(self, _generatedField: list):
+        self.rowAmount: int = int(input("Enter Amount of Rows: "))
+        self.columnAmount: int = int(input("Enter Amount of Items on a Row: "))
         
         if(self.rowAmount):
             self._generatedField = [[None]] * self.rowAmount 
@@ -17,7 +17,13 @@ class Main:
                 for i in range(0, self.rowAmount):  
                     self._generatedField[i] = "x" * self.columnAmount
         print(*self._generatedField, sep = "\n")
+        return self._generatedField
         #return print(f"We have {str(self.columnAmount)} and {str(self.rowAmount)}")
+    def generateShip(self):
+        #self.shipAmount: int = int(input("Enter Amount of Ships: "))
+
+         self._generatedField[0] = "asd"
+         print(self._generatedField)
         
     def getField(self):
         return self._generatedField
@@ -29,3 +35,4 @@ class Main:
 main = Main()
 savedField = main.getField()
 main.generateField(savedField)
+main.generateShip()
