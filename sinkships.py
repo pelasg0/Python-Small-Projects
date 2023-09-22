841cd53b8789e36c9d4291b1bb44cc75f9a70f28
pelasg01000 <91074931+pelasg01000@users.noreply.github.com>
2023-09-22 09:26:22 +0200

    Update sinkships.py
    row and column input()

--- sinkships.py
+++ sinkships.py
@@ -4,15 +4,18 @@ class Main:
     _generatedField:[]
     listIndex:int = 0
     
-    def __init__(self) :
+    def __init__(self):
         pass
 
     def generateField(self):
-        pass
-    
+        self.columnAmount = input("Enter Amount of Items on a Row: ")
+        self.rowAmount = input("Enter Amount of Rows: ")
+        
+        return print(f"We have {self.columnAmount} and {self.rowAmount}")
+        
     def getField(self):
         return self._generatedField
 
 
 main = Main()
-print(main.getField())
+print(main.generateField())
