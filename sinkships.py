a29976be79d208f27fef55e73b3fe96f3a854cfb
pelasg01000 <91074931+pelasg01000@users.noreply.github.com>
2023-09-22 09:02:17 +0200

    Update sinkships.py
    trying a new way

--- sinkships.py
+++ sinkships.py
@@ -1,7 +1,7 @@
 
 
 class Main: 
-    _generatedField:[] = [["x"]]
+    _generatedField:[]
     listIndex:int = 0
     
     def __init__(self) :
@@ -9,24 +9,13 @@ class Main:
 
     def generateField(self):
         sideA:int = int(input("How many rows?"))
-        sideB:int = int(input("How many columns?"))
-
-        #do not use dyynamic vars
+        sideB:int = int(input("How many columns?")
         
-        while len(self._generatedField) < sideA:
-            self._generatedField.append(_generatedField[listIndex])
-            self.listIndex += 1 
-
-        while len(globals()["list_" + str(self.listIndex) + "_"]) < sideB:
-            globals()["list_" + str(self.listIndex) + "_"].append("x")
     
     
     def getField(self):
         return self._generatedField
-    '''
-    def getIndex(self):
-        return self.listIndex
-    '''
+
 
 main = Main()
 main.generateField()
