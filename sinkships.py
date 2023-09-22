5b5d3ce1a1f2b902650d56c1dc5cdb7a996c0b0f
pelasg01000 <91074931+pelasg01000@users.noreply.github.com>
2023-09-22 11:22:41 +0200

    Update sinkships.py
    row item generator in progress

--- sinkships.py
+++ sinkships.py
@@ -1,21 +1,31 @@
 
 
 class Main: 
-    _generatedField:[]
+    _generatedField = []
     listIndex:int = 0
     
     def __init__(self):
         pass
 
-    def generateField(self):
-        self.columnAmount = input("Enter Amount of Items on a Row: ")
-        self.rowAmount = input("Enter Amount of Rows: ")
+    def generateField(self, _generatedField):
+        self.columnAmount = int(input("Enter Amount of Items on a Row: "))
+        self.rowAmount = int(input("Enter Amount of Rows: "))
         
-        return print(f"We have {self.columnAmount} and {self.rowAmount}")
+        while self.columnAmount > len(self._generatedField):
+            self._generatedField = [None] * self.columnAmount
+            self._generatedField[self.listIndex] = "x"
+            self.listIndex += 1
+            print("yes")
+        
+        return print(f"We have {str(self.columnAmount)} and {str(self.rowAmount)}")
         
     def getField(self):
         return self._generatedField
+        
+    def getIndex(self): 
+        return self.listIndex
 
 
 main = Main()
-print(main.generateField())
+savedField = main.getField()
+main.generateField(savedField)
