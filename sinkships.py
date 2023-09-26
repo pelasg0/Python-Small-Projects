48a1b92382ae7a505e3f20f2ea90bc9eb8f82c94
pelasg01000 <91074931+pelasg01000@users.noreply.github.com>
2023-09-26 15:55:47 +0200

    Update sinkships.py
    commenters are added working on solution where it doesn't give error when I input something else that's not a input

--- sinkships.py
+++ sinkships.py
@@ -2,28 +2,50 @@
 #imports
 import random
 
+'''
+class representing the whole game
+'''
 class Main: 
+    '''
+    _generatedField: list - the list that stores all the 'ships'
+    _generatedFieldCopy: list - the list that acts as an 'overlay' so you cannot see where the ships are 
+    '''
     _generatedField: list = []
     _generatedFieldCopy: list = []
     
+    '''
+    in case we need a constructor
+    '''
     def __init__(self):
         pass
 
+    '''
+    generates an empty 'ocean'
+    rowAmount: int - how many rows does the field have
+    columnAmount: int - how many columns does the field have
+    '''
     def generateField(self):
         self.rowAmount: int = int(input("Enter Amount of Rows: "))
         self.columnAmount: int = int(input("Enter Amount of Items on a Row: "))
         
-        if(self.rowAmount):
+        if(self.rowAmount and isinstance(self.rowAmount, int)):
             self._generatedField = [[None]] * self.rowAmount 
             self._generatedFieldCopy = [[None]] * self.rowAmount
-        if(self.columnAmount): 
+        if(self.columnAmount and isinstance(self.columnAmount, int)): 
                 for i in range(0, self.rowAmount):  
                     self._generatedField[i] = ['x'] * self.columnAmount
                     self._generatedFieldCopy[i] = ['x'] * self.columnAmount
         print(*self._generatedField, sep = "\n")
-        
+            
         return self._generatedField, self._generatedFieldCopy
-    
+        
+    '''
+    generetes ships that are marked as 'i' in the 'ocean' 
+    index: int - index that counts the iterations of the loop
+    generatedRow: int - where the random row position of the ship is saved
+    generatedColumn: int - where the random column position of the ship is saved
+    generatedPlaces: int - the amount of places in our ocean, if it's smaller than ship amoung -> else:
+    '''
     def generateShip(self):
         index: int = 0 #index for the loop so it counts the iterations
         generatedRow: int
@@ -41,16 +63,18 @@ class Main:
                 index += 1
         else:
             print("There are not enough places for the ships.") 
-        if index == self.shipAmount: #print the ocean
-            #print(*self._generatedField, sep = "\n")
-            pass
-        
-                
+            
         return self._generatedField
     
+    
+    '''
+    checks if there's a ship or not 
+    shipsCounted: int - the amount of ships in the 'ocean' 
+    success: int - the amount of found ships 
+    '''
     def getVariableCont(self):
         shipsCounted: int = 0 
-        success: int = 0
+        success: int = -1
         
         '''
         code for counting the ships
@@ -74,13 +98,14 @@ class Main:
                             print("That's right!")
                     elif self._generatedField[rowIndexValue][columnIndexValue] == 'x':
                         print("That's wrong.")
-                        #print(*self._generatedField, sep = "\n")
-   
+    
+    '''
+    getter and setter functions used for debugging or testing purposes mostly
+    '''
     def getField(self):
         return self._generatedField
     def getCopy(self): 
         return self._generatedFieldCopy
-        
     def getIndex(self): 
         return self.listIndex
 
