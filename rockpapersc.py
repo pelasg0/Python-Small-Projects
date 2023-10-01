c832cf1347415691fb39c71e05aa5206290bc248
mrdrawin <mrdrawin@abv.bg>
2023-10-01 18:21:24 +0200

    get user input

--- rockpapersc.py
+++ rockpapersc.py
@@ -1,11 +1,54 @@
 '''
 imports
 '''
-import random
+import sys,time,random
 
 
 class main: 
-    signs: list
+    _savedInput: int 
+    _startSymbols: list = [''' 
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+''', 
+'''
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+''',
+'''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+''']
+    
+    _signs: list = ['rock', 'paper', 'scissors']
+
+    def getUserInput(self): 
+        self._savedInput: int = input('__' + '(1): ' + self._startSymbols[0] + '__' +  '(2): ' +  self._startSymbols[1] + '__' +  '(3): ' +  self._startSymbols[2] + 'Which one do you choose? ')
+        
+
+        return self._savedInput
+
+    def getInput(self):
+        return self._savedInput
+    
+main = main()
+sign = main._signs
+main.getUserInput()
+#print(main.getInput())
+
+
+
 
 '''
 ref link : https://gist.github.com/wynand1004/b5c521ea8392e9c6bfe101b025c39abe
