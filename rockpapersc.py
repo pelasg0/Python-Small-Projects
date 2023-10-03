2efd14f7ce5724b716f0e24c681ddf0a98dda6d1
mrdrawin <mrdrawin@abv.bg>
2023-10-03 15:55:24 +0200

    game is done only comments are missing

--- rockpapersc.py
+++ rockpapersc.py
@@ -4,8 +4,8 @@ imports
 import sys,time,random
 
 
-class main: 
-    _savedUserInput: int = 1
+class Main: 
+    _savedUserInput: int
     _generatedPcInput: int 
     _startSymbols: list = [''' 
     _______
@@ -34,70 +34,33 @@ class main:
     
     def genUserInput(self): 
         self._savedUserInput = int(input('__' + '(1): ' + self._startSymbols[0] + '__' +  '(2): ' +  self._startSymbols[1] + '__' +  '(3): ' +  self._startSymbols[2] + 'Which one do you choose? '))
-        #print(self._startSymbols[self._savedUserInput - 1])
-        print(self._savedUserInput)
+        print(self._startSymbols[self._savedUserInput - 1])
         return self._savedUserInput
+    
     def genPcInput(self): 
         self._generatedPcInput = random.randint(1, 3)
-        #print(self._startSymbols[self._generatedPcInput - 1])
-        print(self._savedUserInput)
+        print(self._startSymbols[self._generatedPcInput - 1])
         return self._generatedPcInput
+    
     def compareInputs(self):
-        if self._savedUserInput == 1 and self._generatedPcInput == 3 or self._savedUserInput == 2 and self._generatedPcInput == 1 or self._savedUserInput == 3 and self._generatedPcInput == 1:
+        if self._savedUserInput == 1 and self._generatedPcInput == 3 or self._savedUserInput == 2 and self._generatedPcInput == 1 or self._savedUserInput == 3 and self._generatedPcInput == 2:
             print("User Won")
-        elif self._generatedPcInput == 1 and self._savedUserInput == 3 or self._generatedPcInput == 2 and self._savedUserInput == 1 or self._generatedPcInput == 3 and self._savedUserInput == 1:
+        elif self._generatedPcInput == 1 and self._savedUserInput == 3 or self._generatedPcInput == 2 and self._savedUserInput == 1 or self._generatedPcInput == 3 and self._savedUserInput == 2:
             print("Pc Won")
         else:
             print("Draw")
+
     def getUserInput(self):
         return self._savedUserInput
     
     def getPcInput(self):
         return self._generatedPcInput
+    
 
+def play():
+    main = Main()
+    main.genUserInput()
+    main.genPcInput()
+    main.compareInputs()
 
-main = main()
-main.genUserInput()
-main.genPcInput()
-main.compareInputs()
-
-
-#print(main.getUserInput())
-
-
-
-
-'''
-ref link : https://gist.github.com/wynand1004/b5c521ea8392e9c6bfe101b025c39abe
-# Rock Paper Scissors ASCII Art
-
-# Rock
-print("""
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-""")
-
-# Paper
-print("""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-""")
-
-# Scissors
-print("""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-""")
-'''
+play()
