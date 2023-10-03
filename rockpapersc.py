570e0f777fdb0f282846615ac76fc7ca0672463f
mrdrawin <mrdrawin@abv.bg>
2023-10-03 12:17:21 +0200

    comparison with symbols, vars are taken but false results

--- rockpapersc.py
+++ rockpapersc.py
@@ -32,27 +32,30 @@ class main:
 ---.__(___)
 ''']
     
-    _signs: list = ['rock', 'paper', 'scissors']
-
     def genUserInput(self): 
-        self._savedUserInput = input('__' + '(1): ' + self._startSymbols[0] + '__' +  '(2): ' +  self._startSymbols[1] + '__' +  '(3): ' +  self._startSymbols[2] + 'Which one do you choose? ')
+        self._savedUserInput = int(input('__' + '(1): ' + self._startSymbols[0] + '__' +  '(2): ' +  self._startSymbols[1] + '__' +  '(3): ' +  self._startSymbols[2] + 'Which one do you choose? '))
+        #print(self._startSymbols[self._savedUserInput - 1])
+        print(self._savedUserInput)
         return self._savedUserInput
-
     def genPcInput(self): 
         self._generatedPcInput = random.randint(1, 3)
+        #print(self._startSymbols[self._generatedPcInput - 1])
+        print(self._savedUserInput)
         return self._generatedPcInput
     def compareInputs(self):
         if self._savedUserInput == 1 and self._generatedPcInput == 3 or self._savedUserInput == 2 and self._generatedPcInput == 1 or self._savedUserInput == 3 and self._generatedPcInput == 1:
             print("User Won")
         elif self._generatedPcInput == 1 and self._savedUserInput == 3 or self._generatedPcInput == 2 and self._savedUserInput == 1 or self._generatedPcInput == 3 and self._savedUserInput == 1:
             print("Pc Won")
-
+        else:
+            print("Draw")
     def getUserInput(self):
         return self._savedUserInput
     
     def getPcInput(self):
         return self._generatedPcInput
-    
+
+
 main = main()
 main.genUserInput()
 main.genPcInput()
