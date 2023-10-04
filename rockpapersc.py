c14a97dccf352d20e0e535d96e386bd283fc7216
pelasg01000 <91074931+pelasg01000@users.noreply.github.com>
2023-10-04 08:17:55 +0200

    Update rockpapersc.py
    comments

--- rockpapersc.py
+++ rockpapersc.py
@@ -1,9 +1,15 @@
 '''
-imports
+    imports
+    some of them are not used
 '''
 import sys,time,random
 
-
+'''
+    main class where everything is written
+    _saveUserInput: int - a variable where the user input is saved
+    _generatedPcInput: int - a variable where the pc 'input is saved'
+    _startSymbols: list - a variable where all the ascii symbols are saved
+'''
 class Main: 
     _savedUserInput: int
     _generatedPcInput: int 
@@ -31,17 +37,26 @@ class Main:
       (____)
 ---.__(___)
 ''']
-    
+
+    '''
+        function for getting the actual user input
+    '''
     def genUserInput(self): 
         self._savedUserInput = int(input('__' + '(1): ' + self._startSymbols[0] + '__' +  '(2): ' +  self._startSymbols[1] + '__' +  '(3): ' +  self._startSymbols[2] + 'Which one do you choose? '))
         print(self._startSymbols[self._savedUserInput - 1])
         return self._savedUserInput
-    
+
+    '''
+        function for generating the actual pc input
+    '''
     def genPcInput(self): 
         self._generatedPcInput = random.randint(1, 3)
         print(self._startSymbols[self._generatedPcInput - 1])
         return self._generatedPcInput
-    
+
+    '''
+        function which compares both pc and user 'inputs' and defines who the winner is
+    '''
     def compareInputs(self):
         if self._savedUserInput == 1 and self._generatedPcInput == 3 or self._savedUserInput == 2 and self._generatedPcInput == 1 or self._savedUserInput == 3 and self._generatedPcInput == 2:
             print("User Won")
@@ -49,14 +64,18 @@ class Main:
             print("Pc Won")
         else:
             print("Draw")
-
+    '''
+        some getter and setter methods for testing purposes
+    '''
     def getUserInput(self):
         return self._savedUserInput
     
     def getPcInput(self):
         return self._generatedPcInput
     
-
+'''
+    a script where everything is 'played'
+'''
 def play():
     main = Main()
     main.genUserInput()
