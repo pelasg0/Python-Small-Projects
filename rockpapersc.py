7f5f57a671b5166e3c3a4b9fd947996cd890b370
pelasg01000 <91074931+pelasg01000@users.noreply.github.com>
2023-10-05 07:47:06 +0200

    Update rockpapersc.py

--- rockpapersc.py
+++ rockpapersc.py
@@ -1,14 +1,14 @@
 '''
-    imports
-    some of them are not used
+imports
+some of them are not used
 '''
 import sys,time,random
 
 '''
-    main class where everything is written
-    _saveUserInput: int - a variable where the user input is saved
-    _generatedPcInput: int - a variable where the pc 'input is saved'
-    _startSymbols: list - a variable where all the ascii symbols are saved
+main class where everything is written
+_saveUserInput: int - a variable where the user input is saved
+_generatedPcInput: int - a variable where the pc 'input is saved'
+_startSymbols: list - a variable where all the ascii symbols are saved
 '''
 class Main: 
     _savedUserInput: int
@@ -39,7 +39,7 @@ class Main:
 ''']
 
     '''
-        function for getting the actual user input
+    function for getting the actual user input
     '''
     def genUserInput(self): 
         self._savedUserInput = int(input('__' + '(1): ' + self._startSymbols[0] + '__' +  '(2): ' +  self._startSymbols[1] + '__' +  '(3): ' +  self._startSymbols[2] + 'Which one do you choose? '))
@@ -47,7 +47,7 @@ class Main:
         return self._savedUserInput
 
     '''
-        function for generating the actual pc input
+    function for generating the actual pc input
     '''
     def genPcInput(self): 
         self._generatedPcInput = random.randint(1, 3)
@@ -55,7 +55,7 @@ class Main:
         return self._generatedPcInput
 
     '''
-        function which compares both pc and user 'inputs' and defines who the winner is
+    function which compares both pc and user 'inputs' and defines who the winner is
     '''
     def compareInputs(self):
         if self._savedUserInput == 1 and self._generatedPcInput == 3 or self._savedUserInput == 2 and self._generatedPcInput == 1 or self._savedUserInput == 3 and self._generatedPcInput == 2:
@@ -65,7 +65,7 @@ class Main:
         else:
             print("Draw")
     '''
-        some getter and setter methods for testing purposes
+    some getter and setter methods for testing purposes
     '''
     def getUserInput(self):
         return self._savedUserInput
@@ -74,7 +74,7 @@ class Main:
         return self._generatedPcInput
     
 '''
-    a script where everything is 'played'
+a script where everything is 'played'
 '''
 def play():
     main = Main()
